68085a150acde2f3ef8ef1526b2f2969d34b9631
Christian Monrad Mortensen <chrmonmor@gmail.com>
2022-12-07 18:29:15 +0100

    Er ikke i stand til at arbejde på det her projekt her hjemme da min computer ikke kan køre programmet, eller der er noget galt med Visual Studio. men jeg ved ikke hvad.

--- ScrumbanAPI/Program.cs
+++ ScrumbanAPI/Program.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+
 
 internal class Program
 {
+    // tenperary placement 
+    private static readonly string _con = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ToemreDate;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +19,9 @@ internal class Program
         // Configure the HTTP request pipeline.
 
         app.UseHttpsRedirection();
+
+        SqlConnection connection = new SqlConnection(_con);
+        SqlCommand command = new SqlCommand("Select * from Vare", connection);
 
         app.MapGet("/workers/all", () =>
         {
